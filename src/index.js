54e589849a8ad388086c3e66d4ba2eefdedc6d6a
Adam Nguyen <Adanatonw@gmail.com>
2023-06-26 22:20:59 -0400

    First mini project done by myself

--- src/index.js
+++ src/index.js
@@ -1,17 +1,70 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        {/* Should contain one Skill component
+        for each web dev skill that you have,
+        customized with props */}
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+function Avatar() {
+  return (
+    <img src="rabbit.jpg" alt="rabbit picture" style={{ width: "442px" }}></img>
+  );
+}
+function Intro() {
+  return (
+    <React.Fragment>
+      <h2>Adam Nguyen</h2>
+      <p>
+        Hello I'm Adam. I like making coding projects to improve my skills.
+        Rabbits are one of my favorite animals. This summer I'm learning React
+        to complement my frontend skills. Hopefully I'll learn node.js and
+        operate apis on the backend with an sql database.
+      </p>
+    </React.Fragment>
+  );
+}
+
+function SkillList() {
+  return (
+    <ul className="skill-list">
+      <Skill name="HTML" />
+      <Skill name="CSS" />
+      <Skill name="JavaScript" />
+      <Skill name="React" />
+      <Skill name="Node.Js" />
+      <Skill name="Express" />
+    </ul>
+  );
+}
+
+function Skill(props) {
+  const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+  const style = { backgroundColor: randomColor };
+
+  return (
+    <li className="skill" style={style}>
+      {props.name}
+    </li>
+  );
+}
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
